2f0fcf223e2e6f2c8944d8c811fffb3fe8e2f9e0
unknown <eze.rey92@gmail.com>
2021-03-20 18:08:45 -0300

    ultima modificacion

--- db.js
+++ db.js
@@ -1,23 +1,28 @@
 const  Sequelize  = require("sequelize");
+const productos = require("./models/productos");
 
 const ProductoModel = require("./models/productos");
+const UserModel = require("./models/users");
 const path = 'mysql://root@localhost:3306/prueba';
 
 
 
 
 
-const sequelize = new Sequelize(path,{
+const sequelize_ruta = new Sequelize(path,{
     host:"localhost",
     dialect: "mssql"
 
 })
 
-const Producto = ProductoModel(sequelize,Sequelize);
-sequelize.sync({force :false})
+const Producto = ProductoModel(sequelize_ruta,Sequelize);
+const User = UserModel(sequelize_ruta,Sequelize);
+
+sequelize_ruta.sync({force :false})
 .then(()=>{
     console.log("tablas sincronizadas");
 })
 module.exports ={
-    Producto
+    Producto,
+    User
 }
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 
 const apiProductos = require("./api/productos");
+const apiUsers = require("./api/users");
+
 
 router.use("/productos",apiProductos);
+router.use("/users",apiUsers);
 
 module.exports = router;
