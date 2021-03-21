402462e9cf4bf3c4e111d81e2689016885726910
unknown <eze.rey92@gmail.com>
2021-03-21 01:43:05 -0300

    validacion username , email

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,25 @@
 const router= require("express").Router();
 const bcrypt = require("bcryptjs");
-const {User} = require("../../db")
+const {User} = require("../../db");
+const {check, validationResult} =require("express-validator");
 
 
 router.post("/registrer",[
+check("username","El username es obligatorio").not().isEmpty(),
+check("email","El email es obligatorio").not().isEmpty(),
+],
+ 
+
+async (req,res)=>{
+
+
+const errors = validationResult(req);
+if (!errors.isEmpty()) {
+    return res.status(422).json({errores:errors.array()})
     
-], async (req,res)=>{
+}
+
+
    // req.body.password =  bcrypt.hashSync(req.body.password,10);
     const user =  await User.create(req.body);
     res.json(user);
