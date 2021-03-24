c12198ff32ebca3ea60faeaf78cf3f5689890b52
unknown <eze.rey92@gmail.com>
2021-03-23 22:05:34 -0300

    un poco de front para hacer mas rapido las peticiones

--- front/front crear plato.js
+++ front/front crear plato.js
@@ -1,5 +1,10 @@
-const formularioCrearPlato    =document.getElementById("formulario-crear-plato");
-const btnCrearPlato  =document.getElementById("btn-crear-plato");
+
+
+const formularioCrearPlato            =document.getElementById("formulario-crear-plato");
+const btnCrearPlato                   =document.getElementById("btn-crear-plato");
+const obtenerTodosLosPlatosDisponibles= document.getElementById("obtener-todos-los-platos");
+const contenedorTodosLosPlatos        =document.getElementById("contenedor-todos-los-platos");
+var todoLosPlatosArray =[];
 var url = 'http://localhost:3000/api/productos';
 
 
@@ -22,12 +27,33 @@ btnCrearPlato.addEventListener("click",(ev)=>{
     plato.descripcion    = descripcion;
 
 
-    creandoUsuario(plato)
+    creandoPlato(plato)
     console.log(plato,"este es mi usuario");
     formularioCrearPlato.reset();
 })
 
-function creandoUsuario(plato) {
+obtenerTodosLosPlatosDisponibles.addEventListener("click",(ev)=>{
+  obtenerTodosLosPlatos();
+      var JSONString2 =JSON.stringify(todoLosPlatosArray) ;
+      var pasadoAObj = JSON.parse(JSONString2);
+
+      for (let index = 0; index < todoLosPlatosArray.length; index++) {
+      console.log(pasadoAObj[index])
+      contenedorTodosLosPlatos.innerHTML +=`<div><p>Nombre: ${pasadoAObj[index].nombre}</p> <p>Precio: ${pasadoAObj[index].precio}</p> <p>Descripción: ${pasadoAObj[index].descripcion}</p> <p>Creado: ${pasadoAObj[index].createdAt}</p></div>` 
+        
+      }
+
+})
+
+
+
+
+
+
+
+
+
+function creandoPlato(plato) {
     fetch(url, {
         method: 'POST', // or 'PUT'
         body: JSON.stringify(plato), 
@@ -39,3 +65,11 @@ function creandoUsuario(plato) {
       .then(response => console.log('Success:', response));
 }
 
+function obtenerTodosLosPlatos() {
+  fetch(url)
+  .then(response => response.json())
+ // .then((data) => console.log(data))
+  .then((data) => todoLosPlatosArray=data)
+
+  
+}
