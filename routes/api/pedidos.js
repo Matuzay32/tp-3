51edc882443ade12c598873cf24f51573295c4e8
unknown <eze.rey92@gmail.com>
2021-04-03 20:08:32 -0300

    HACIENDO ENDPOINTS DE PEDIDOS

--- routes/api/pedidos.js
+++ routes/api/pedidos.js
@@ -7,7 +7,35 @@ router.get("/", async(req,res)=>{
     res.json(pedidos);
 });
 
+router.post("/",async (req,res)=>{
+
+    const pedido = await Pedido.create(req.body);
+    res.json(pedido);
+    
+    
+});
 
 
+router.put("/:pedidoId",async (req,res)=>{
+
+        await Pedido.update(req.body,{
+            where:{id: req.params.pedidoId}
+        });
+        res.json({succcess: "update correcto"});
+        
+        
+});
+
+
+router.delete("/:pedidoId",async (req,res)=>{
+
+        await Producto.destroy({
+            where:{id: req.params.pedidoId}
+        });
+        res.json({succcess: "producto borrado"});
+        
+        
+});
+     
 
 module.exports = router;
