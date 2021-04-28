121ffe811c0da104e85bb7d6a29c1e365b4a22a1
unknown <eze.rey92@gmail.com>
2021-04-27 22:38:32 -0300

    cambie el nombre a la base de datos

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const  Sequelize  = require("sequelize");
 const PedidoModel = require("./models/pedido");
 const ProductoModel = require("./models/productos");
 const UserModel = require("./models/users");
-const path = 'mysql://root@localhost:3306/prueba';
+const path = 'mysql://root@localhost:3306/delilah resto';
 
 
 
